ba447134774f19deea67d2017dea8580f243c7f3
Manuelklly <Manuelkelly777@gmail.com>
2024-07-01 15:35:11 -0300

    last commit

--- src/main/java/edu/fiuba/algo3/Vista/VistaInicioDeJuego.java
+++ src/main/java/edu/fiuba/algo3/Vista/VistaInicioDeJuego.java
@@ -1,7 +1,6 @@
 package edu.fiuba.algo3.Vista;
 
 import edu.fiuba.algo3.Utilidades.ObservableConcreto;
-import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -44,7 +43,7 @@ public class VistaInicioDeJuego extends ObservableConcreto {
 
         // Título con efecto neón rosa
         titulo_l.setText("ALGOKAHOOT");
-        titulo_l.setStyle("-fx-text-fill: #FF69B4;");
+        titulo_l.setStyle("-fx-text-fill: black;-fx-font-weight: bold;");
         if (neonFont != null) {
             titulo_l.setFont(neonFont);
         } else {
@@ -56,12 +55,12 @@ public class VistaInicioDeJuego extends ObservableConcreto {
         ds.setSpread(0.65);
         ds.setColor(Color.rgb(255, 105, 180, 1));
         ds.setRadius(100);
-        titulo_l.setEffect(ds);
+        // titulo_l.setEffect(ds);
 
         subt.setText("[  Selecciona un modo de juego  ]");
         subt.setStyle("-fx-font-family: 'Comic Sans MS';" +
                 "-fx-font-size: 32px;" +
-                "-fx-text-fill: #EDD3C4;");
+                "-fx-text-fill: black;");
         subt.setAlignment(Pos.CENTER);
 
         cerrar_b.setOnAction(e -> stage.close());
@@ -76,20 +75,41 @@ public class VistaInicioDeJuego extends ObservableConcreto {
             stage.close();
         });
 
-        // Estilo para los botones con esquinas redondeadas
-        String buttonStyle = "-fx-background-color: rgba(20, 117, 110, 0.7); -fx-text-fill: #EDD3C4; -fx-font-family: 'Comic Sans MS'; -fx-font-size: 24px; -fx-background-radius: 20;";
+        // Estilo para los botones según .button-54 con el nuevo color de relleno
+        String buttonStyle = "-fx-font-family: 'Open Sans', sans-serif;" +
+                "-fx-font-size: 20px;" +
+                "-fx-letter-spacing: 2px;" +
+                "-fx-text-decoration: none;" +
+                "-fx-text-transform: uppercase;" +
+                "-fx-text-fill: black;-fx-font-weight: bold;"+
+                "-fx-cursor: pointer;" +
+                "-fx-border-width: 3px;" +
+                "-fx-border-color: black;" +
+                "-fx-padding: 0.25em 0.5em;" +
+                "-fx-effect: dropshadow(gaussian, black, 1, 0, 1, 1);" +
+                "-fx-position: relative;" +
+                "-fx-user-select: none;" +
+                "-webkit-user-select: none;" +
+                "-fx-touch-action: manipulation;" +
+                "-fx-opacity: 0.9;"+"-fx-background-color: #358f89;";
 
         PartidaRapida.setStyle(buttonStyle);
         PartidaIntensiva.setStyle(buttonStyle);
         cerrar_b.setStyle(buttonStyle);
 
-        // Agregar manejadores de eventos para cambiar la opacidad
+        // Añadir manejadores de eventos para hover y active
+        PartidaRapida.setOnMousePressed(e -> PartidaRapida.setStyle(buttonStyle + "-fx-effect: dropshadow(gaussian, black, 0, 0, 0, 0); -fx-translate-x: 5px; -fx-translate-y: 5px;"));
+        PartidaRapida.setOnMouseReleased(e -> PartidaRapida.setStyle(buttonStyle));
         PartidaRapida.setOnMouseEntered(e -> PartidaRapida.setOpacity(1.0));
         PartidaRapida.setOnMouseExited(e -> PartidaRapida.setOpacity(0.7));
 
+        PartidaIntensiva.setOnMousePressed(e -> PartidaIntensiva.setStyle(buttonStyle + "-fx-effect: dropshadow(gaussian, black, 0, 0, 0, 0); -fx-translate-x: 5px; -fx-translate-y: 5px;"));
+        PartidaIntensiva.setOnMouseReleased(e -> PartidaIntensiva.setStyle(buttonStyle));
         PartidaIntensiva.setOnMouseEntered(e -> PartidaIntensiva.setOpacity(1.0));
         PartidaIntensiva.setOnMouseExited(e -> PartidaIntensiva.setOpacity(0.7));
 
+        cerrar_b.setOnMousePressed(e -> cerrar_b.setStyle(buttonStyle + "-fx-effect: dropshadow(gaussian, black, 0, 0, 0, 0); -fx-translate-x: 5px; -fx-translate-y: 5px;"));
+        cerrar_b.setOnMouseReleased(e -> cerrar_b.setStyle(buttonStyle));
         cerrar_b.setOnMouseEntered(e -> cerrar_b.setOpacity(1.0));
         cerrar_b.setOnMouseExited(e -> cerrar_b.setOpacity(0.7));
 
@@ -113,9 +133,11 @@ public class VistaInicioDeJuego extends ObservableConcreto {
         // Añadir VBox encima de las formas
         root.getChildren().add(vbox);
 
-        // Centrar VBox en el medio de la escena
-        root.widthProperty().addListener((obs, oldVal, newVal) -> vbox.setLayoutX((newVal.doubleValue() - vbox.getWidth()) / 2));
-        root.heightProperty().addListener((obs, oldVal, newVal) -> vbox.setLayoutY((newVal.doubleValue() - vbox.getHeight()) / 2));
+        // Centrar VBox en el medio de la escena cuando se muestra por primera vez y mientras se cambia el tamaño
+        vbox.layoutBoundsProperty().addListener((obs, oldBounds, newBounds) -> {
+            vbox.setLayoutX((root.getWidth() - newBounds.getWidth()) / 2);
+            vbox.setLayoutY((root.getHeight() - newBounds.getHeight()) / 2);
+        });
 
         Scene scene = new Scene(root, minSceneWidth, minSceneHeight);
 
@@ -128,6 +150,10 @@ public class VistaInicioDeJuego extends ObservableConcreto {
 
         // Añadir formas aleatorias al fondo
         addRandomShapes(root, 30);
+
+        // Asegurar que el VBox esté centrado al mostrarse la escena por primera vez
+        stage.widthProperty().addListener((obs, oldVal, newVal) -> vbox.setLayoutX((root.getWidth() - vbox.getWidth()) / 2));
+        stage.heightProperty().addListener((obs, oldVal, newVal) -> vbox.setLayoutY((root.getHeight() - vbox.getHeight()) / 2));
     }
 
     private void addRandomShapes(Pane pane, int numberOfShapes) {
